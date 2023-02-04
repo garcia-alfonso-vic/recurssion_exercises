2c77157ab268b16beb1f108c97d56a68d09279f9
garcia, victor <vgarcia87@go.pasadena.edu>
2023-02-03 16:26:52 -0800

    updated function.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <cstdlib>
 #include <iostream>
 #include <iomanip>
-void R1_levls(unsigned int level);
+using namespace std;
+void R1_levls(unsigned int max_level, int start_level = 1);
 void R2_Box(); 
 void R3_first_second();
 int main()
 {
-    using namespace std;
+
     int level = 4, space = 1, write_me_vert = -1234; 
     R1_levls(level);
     
@@ -22,23 +23,19 @@ This was written by call number 1.
    This ALSO written by call number 3.
   This ALSO written by call number 2.
  This ALSO written by call number 1.*/
-void R1_levls(unsigned int level)
-{
-    if (level >= 1)
+void R1_levls(unsigned int max_level, int start_level )
+{   
+    if (max_level > 1)
     {
-        std::cout << "This was written by call number " << level << ".\n";
-        R1_levls(level - 1);
         
-        std::cout << "This ALSO written by call number " << level << ".\n";
+        R1_levls(max_level - 1);
+        for (int i = 0; i < max_level; i++)
+            cout << ' ';
+        cout << "This was written by call number " << max_level << endl;
     }
-    else 
-    {
-        return;
-    }
-    
-    
-
-}   
+    else
+        cout << "This was written by call number " << max_level << endl;
+}
     
 void R2_Box()
 {
