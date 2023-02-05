829dae046b77a732392f6330889b7263905b1a2c
garcia, victor <vgarcia87@go.pasadena.edu>
2023-02-05 09:07:33 -0800

    added Box function.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <cstdlib>
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
 void R1_levls(unsigned int max_level, int start_level = 1);
-void R2_Box(); 
+void R2_Box(string prefix, unsigned int levels); 
 void R3_first_second();
 int main()
 {
 
     int level = 4, space = 1, write_me_vert = -1234; 
     R1_levls(level);
+    R2_Box("Box:", 2);
     
     return EXIT_SUCCESS;
 }
@@ -25,40 +27,49 @@ This was written by call number 1.
  This ALSO written by call number 1.*/
 void R1_levls(unsigned int max_level, int start_level )
 {   
+    //condition that allows recurssion
     if (start_level < max_level)
     { 
+        //used to print spaces
         for (int i = 0; i < start_level; i++)
             cout << ' ';
+        //writes from 0 - 4, each step BEFORE recurssive function
         cout << "This was written by call number " << start_level << endl;
+        // were the fun happens...
         R1_levls(max_level, start_level + 1);
+        // used to print spaces
         for (int i = 0; i < start_level; i++)
             cout << ' ';
+        // AFTER recurssion ends, writes this from 4 - 1
         cout << "This ALSO written by call number " << start_level << endl;
     }
     else 
-    {  
+    {  //used to print spaces
         for (int i = 0; i < start_level; i++)
             cout << ' ';
+        // last conditions
         cout << "This was written by call number " << start_level << endl;
+        
         for (int i = 0; i < start_level; i++)
             cout << ' ';
         cout << "This ALSO written by call number " << start_level << endl;
     }
         
 }
-    // if (max_level > 1)
-    // {
-        
-    //     R1_levls(max_level - 1);
-    //     for (int i = 0; i < max_level; i++)
-    //         cout << ' ';
-    //     cout << "This was written by call number " << max_level << endl;
-    // }
-    // else
-    //     cout << "This was written by call number " << max_level << endl;
-
     
-void R2_Box()
+void R2_Box(string prefix, unsigned int levels)
 {
-
+    if (levels > 1)
+    {   
+        R2_Box(prefix, levels - 1);
+        
+    }
+    else 
+    {
+    for (unsigned int i = 1; i <= 9; i++)
+    {
+        for(unsigned int j = 1; j <= 9; j++)
+            cout << prefix << i << "." << j << endl;
+    }
+    }
 }
